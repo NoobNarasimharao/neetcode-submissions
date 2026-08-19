c8f9dd7b3c55479485dbf226820cde9dd5a12322
Narasimharao <grimmjowjaegerjaquez007@gmail.com>
2026-08-19 11:14:10 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        import random
+
+        def swap(arr, i, j):
+            arr[i], arr[j] = arr[j], arr[i]
+
+        def partition(arr, low, high):
+            pivot_index = random.randint(low, high)
+            swap(arr, pivot_index, high)
+
+            pivot = arr[high]
+            i = low - 1
+
+            for j in range(low, high):
+                if arr[j] < pivot:
+                    i += 1
+                    swap(arr, i, j)
+
+            swap(arr, i + 1, high)
+            return i + 1
+
+        def quickSort(arr, low, high):
+            if low < high:
+                pi = partition(arr, low, high)
+                quickSort(arr, low, pi - 1)
+                quickSort(arr, pi + 1, high)
+
+        quickSort(nums, 0, len(nums) - 1)
